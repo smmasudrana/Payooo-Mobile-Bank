e3ab1caae91f7fc59ab9cea3d5a5b0bf4b435956
smmasudrana <smmasudrana62@gmail.com>
2025-08-26 22:57:53 +0600

    add transfer money and get bonush functionality

--- home.js
+++ home.js
@@ -25,8 +25,19 @@ const availableBalanceElement = document-getElementById("available-balance")
 availableBalanceElement.innerText = value
 }
 
+//reuseable function for toggle to hiding parent
+function hideParentWithToggle(id){
+    const forms = document.getElementsByClassName('form');
+    for(const form of forms){
+        form.style.display='none';
+    }
+    document.getElementById(id).style.display = 'block';
+}
 
-//logout button functionality
+//-----------------------------------------------------------------------------------------------------------
+
+
+//logout button functionality-----------------------------------------------------
 document.getElementById('log-out-btn').addEventListener('click', function(){
     window.location.href="./index.html";
 })
@@ -59,6 +70,7 @@ document.getElementById('addMoneyBtn')
     document.getElementById('available-balance').innerText = totalNewAvailableBalance;
 })
 
+
 //cashout money functionality ------------------------------------------
 document.getElementById('withdraw-btn').addEventListener('click', function(event){
     event.preventDefault();
@@ -82,17 +94,94 @@ document.getElementById('withdraw-btn').addEventListener('click', function(event
 })
 
 
-//toggling feature -------------------------------------------------
+//transfer money functionality
+document.getElementById('sendNow-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const transferAccountNumber = getInputValue('transfer-acNumber');
+    const transferAmount = getInputValueNumber('transfer-amount');
+    const transferPin = getInputValueNumber('transfer-pin')
+
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+
+    const availableBalanceAfterTransfer = availableBalance - transferAmount;
+    document.getElementById('available-balance').innerText = availableBalanceAfterTransfer;
+
+})
+
+//get bonus functionality
+document.getElementById('getBonus-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const validBonusCoupon = 'masud10';
+    const bonusCoupon = getInputValue('bonus-coupon');
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+
+    if(validBonusCoupon === bonusCoupon){
+        const afterBonus = availableBalance + 500;
+        document.getElementById('available-balance').innerText = afterBonus;
+    }
+    else{
+        alert('please enter valid coupon code');
+    }
+})
+
+
+//toggling feature hide parent-------------------------------------------------
 
 document.getElementById('add-money-btn').addEventListener('click', function(){
-    document.getElementById('cashout-parent').style.display = 'none';
+    // const forms = document.getElementsByClassName('form');
+    // for(const form of forms){
+    //     form.style.display='none';
+    // }
+    // document.getElementById('add-money-parent').style.display = 'block';
 
-    document.getElementById('add-money-parent').style.display = 'block';
+    hideParentWithToggle('add-money-parent');
+
+    const formBoxs = document.getElementsByClassName('form-box');
+    for(const formBox of formBoxs){
+        formBox.classList.remove('border-[#e6e6e6]')
+        formBox.classList.add('border-[#e6e6e6]')
+    }
+    document.getElementById('add-money-btn').classList.remove('border-[#e6e6e6]')
+    document.getElementById('add-money-btn').classList.add('border-[#0874f2]', 'bg-[#f2f8fe]');
+    
 })
-
 
 document.getElementById('cashout-btn').addEventListener('click', function(){
-    document.getElementById('add-money-parent').style.display = 'none';
+    hideParentWithToggle('cashout-parent');
 
-    document.getElementById('cashout-parent').style.display = 'block';
+    const formBoxs = document.getElementsByClassName('form-box');
+    for(const formBox of formBoxs){
+        formBox.classList.remove('border-[#e6e6e6]')
+        formBox.classList.add('border-[#e6e6e6]')
+    }
+    document.getElementById('cashout-btn').classList.remove('border-[#e6e6e6]')
+    document.getElementById('cashout-btn').classList.add('border-[#0874f2]', 'bg-[#f2f8fe]')
 })
+
+document.getElementById('transfer-btn').addEventListener('click', function(){
+    hideParentWithToggle('transfer-parent');
+
+    const formBoxs = document.getElementsByClassName('form-box');
+    for(const formBox of formBoxs){
+        formBox.classList.remove('border-[#e6e6e6]')
+        formBox.classList.add('border-[#e6e6e6]')
+    }
+    document.getElementById('transfer-btn').classList.remove('border-[#e6e6e6]')
+    document.getElementById('transfer-btn').classList.add('border-[#0874f2]', 'bg-[#f2f8fe]');
+})
+
+document.getElementById('btn-for-bonus').addEventListener('click', function(){
+    hideParentWithToggle('getBonus-parent');
+
+    const formBoxs = document.getElementsByClassName('form-box');
+    for(const formBox of formBoxs){
+        formBox.classList.remove('border-[#e6e6e6]')
+        formBox.classList.add('border-[#e6e6e6]')
+    }
+    document.getElementById('btn-for-bonus').classList.remove('border-[#e6e6e6]')
+    document.getElementById('btn-for-bonus').classList.add('border-[#0874f2]', 'bg-[#f2f8fe]');
+})
+
+
+//toggle box active bg and border color apply
+document.getElementsByClassName
